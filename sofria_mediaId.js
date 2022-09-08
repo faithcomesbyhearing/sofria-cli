6ea68c7ef4791f48776cc8ef63455f0b3919faa0
Jon Stearley <jrstear@fcbhmail.org>
2022-09-08 08:49:42 -0600

    second thoughts

--- sofria_mediaId.js
+++ sofria_mediaId.js
@@ -2,16 +2,14 @@ const path = require('path');
 const fse = require('fs-extra');
 const {Proskomma} = require('proskomma');
 
-// CLI: sofria-mediaIDs <SOURCE DIRECTORY> <FCBH LANGUAGE CODE> <TRANSLATION ACRONYM>
-// last two arguments are defined in 
-// https://docs.google.com/document/d/1qWbrPWoofjh9ltqic6J6ybdMq06TsJIDVnmJ_su3boQ/edit#heading=h.ezbopy32kofh
-// example: sofria-mediaId testdata/ENGESVN_ET ENG ESV  
-// result: create sofria json mediaIDs, specifically:
-//  1. create appropriate mediaID directories in current working directory
-//  2. in the above, create one sofria json file for each usx file found in SOURCE DIRECTORY
+// CLI: sofria-mediaId <USX FILESET DIRECTORY>
+// example: sofria-mediaId testdata/ENGESVN_ET-usx
+// result: create a corresponding sofria json fileset, eg:
+//  1. create testdata/ENGESVN_ET-json/ directory (note that it is adjacent to the SOURCE directory)
+//  2. create one sofria json file therein, for each usx file found in SOURCE
 
 if (process.argv.length !== 5) {
-    throw new Error(`usage: sofria-mediaId <SOURCE DIRECTORY> <FCBH LANGUAGE CODE> <TRANSLATION ACRONYM>`);
+    throw new Error(`usage: sofria-mediaId <USX FILESET DIRECTORY>`);
 }
 
 const pk = new Proskomma();
@@ -26,20 +24,20 @@ const fqPath = xx
 
 const content = fse.readFileSync(fqPath).toString();
 const suffix = fqPath.split(".").reverse()[0];
-// replace xxx with FCBH LANGUAGE CODE; similarly, replace yyy with TRANSLATION ACRONYM
+// replace xxx with first chars 0-2 of USX FILESET DIRECTORY and yyy with chars 3-5
+// note that 0-2 is FCBH language code and 3-5 are the renditionID (which can be distinct per writing system)
+// see https://docs.google.com/document/d/1qWbrPWoofjh9ltqic6J6ybdMq06TsJIDVnmJ_su3boQ/edit#heading=h.ezbopy32kofh for more info
 pk.importDocument({lang: "xxx", abbr: "yyy"}, suffix, content);
+// I assume that xxx and yyy are embedded in the json somewhere?
 const chaptersQuery = `{documents {cIndexes { chapter } } }`;
 const chaptersResult = pk.gqlQuerySync(chaptersQuery);
 const ret = {};
 for (const chapter of chaptersResult.data.documents[0].cIndexes.map(ci => ci.chapter)) {
     const chapterQuery = `{documents {sofria(indent: 2, chapter: ${chapter}) } }`;
-    // FIXME: instead of adding the chapter to a larger document to be returned, write the chapter out to the filesystem
-    // outfile = first seven of directory name (eg ENGESVN) + "_" + book_id + "_" + ${chapter}
-    // filename example: ENGESV_040MAT_1.json
-    // do we want to name json files consistently with audio files?  eg ENGESVN_ET-json/ENGESVN_ET_B001_MAT_001.json
-    // per https://docs.google.com/document/d/1ytVKiyzTXmPsEz170UHTlZ8NXeXxUOrqNgpPurm_AGE/edit#heading=h.y3ecefloodui
-    // upside: consistency.  downside: change from current practice (who cares?).  
-    // bummer: text mediaIDs have a _ which is also the delimiter in our filename convention
+    // FIXME: instead of adding the chapter to a larger document to be returned, write one file per chapter
+    // example: given an input usx file ENGESVN_ET-usx/040MAT.usx, create ENGESVN_ET-json/040_MAT_{chapter}.json
+    // where chapter is a zero-padded three-digit integer, one for each chapter in the input usx file
+    // BTW: it'd be nicer if dbp-etl also separated the book order number and the book code with a _
     chapter_json = JSON.parse(pk.gqlQuerySync(chapterQuery).data.documents[0].sofria);
     // output chapter_json to outfile
 }
